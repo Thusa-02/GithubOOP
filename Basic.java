1ef9060e6c6b77c31b90a9b54d6f9f565524ba17
Thusa-02 <thusanikam@gmail.com>
2025-03-19 21:16:15 +0530

    my file

---
+++ Basic.java
@@ -0,0 +1,14 @@
+
+//this is a comment
+// this is our first java program
+
+import java.io.*; // how we import packages
+class Basic{// class name should start with capital letter
+	public static void main(String[] args){
+		System.out.println("The first java");
+		System.out.print("hello");
+		
+		
+	}
+
+}
